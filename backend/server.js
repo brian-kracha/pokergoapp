fadf105d5246c6b735edfe5b23c810729f049b57
brain-kracha <brian.kracha@gmail.com>
2018-03-08 08:55:43 -0700

    pushing basic server

--- backend/server.js
+++ backend/server.js
@@ -6,11 +6,10 @@ const io = require('socket.io').listen(server)
 let users =[]
 let connections = []
 
+
 server.listen(process.env.PORT || 3000)
 console.log('server running')
 io.sockets.on('connection', socket => {
   connections.push(socket)
   console.log('shut up bishal', connections.length,socket.id);
-
-
 })
